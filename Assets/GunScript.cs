91be2bf1983c595a636e80cb3d7e80ae9622d1d5
Mark <voongm@my.yorku.ca>
2019-08-02 10:06:27 -0400

    Initial Commit

---
+++ Assets/GunScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class GunScript : MonoBehaviour
+{
+    public SteamVR_Action_Boolean Trigger;
+    public SteamVR_Input_Sources hand;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Trigger.AddOnStateDownListener(TriggerDown, hand);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SteamVR_Input.)
+        
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 SampleVRProject
+
+WIP
